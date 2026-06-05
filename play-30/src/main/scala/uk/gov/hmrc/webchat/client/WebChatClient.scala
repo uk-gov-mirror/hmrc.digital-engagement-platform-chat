369dff4a0dec7226188a13039c2494e8b599de04
Paul Olabisi <18728756+paulonevrything@users.noreply.github.com>
2026-06-05 08:24:34 +0100

    updated library for ID&V

--- play-30/src/main/scala/uk/gov/hmrc/webchat/client/WebChatClient.scala
+++ play-30/src/main/scala/uk/gov/hmrc/webchat/client/WebChatClient.scala
@@ -20,22 +20,40 @@ package uk.gov.hmrc.webchat.client
 import play.api.Logging
 import play.api.mvc.Request
 import play.twirl.api.Html
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.http.HeaderCarrierConverter
+import uk.gov.hmrc.webchat.controllers.AuthFunction
 import uk.gov.hmrc.webchat.models.EncryptedNuanceData
 import uk.gov.hmrc.webchat.services.NuanceEncryptionService
 import uk.gov.hmrc.webchat.views.html.{HMRCEmbeddedView, HMRCPopupView, NuanceTagElementView, NuanceView}
 
 import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
 
 class WebChatClient @Inject()(nuanceEncryptionService: NuanceEncryptionService,
                               requiredElements: NuanceView,
                               popupChatSkinElement: HMRCPopupView,
                               embeddedChatSkinElement: HMRCEmbeddedView,
-                              nuanceContainerElement: NuanceTagElementView) extends Logging {
+                              nuanceContainerElement: NuanceTagElementView,
+                              val authConnector: AuthConnector)(implicit ec: ExecutionContext) extends AuthFunction with Logging {
 
   def loadRequiredElements()(implicit request: Request[_]): Option[Html] = {
     Some(withCSPNonce(requiredElements(encryptedNuanceData)))
   }
+  
+  def loadRequiredElementsWIthAuth()(implicit request: Request[_]): Future[Option[Html]] ={
+    implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
+    
+    retrieveUserProfile()
+      .map {
+      profile => 
+        logger.info(s"Retrieved webchat user profile: ${profile.toLogString}")
+        Some(withCSPNonce(requiredElements(encryptedNuanceData)))
+    }
+  }
+  
+  
   def loadHMRCChatSkinElement(partialType: String, id: String = "")(implicit request: Request[_]): Option[Html] = {
     partialType match {
       case "popup" => Some(withCSPNonce(popupChatSkinElement(id)))
