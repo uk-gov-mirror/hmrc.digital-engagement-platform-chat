74e543565cf1a4c4cebf6acf75808f4edcd7c598
charultripathi26 <charultripathi26@gmail.com>
2026-08-07 15:40:21 +0100

    reverting to scala version 2.13.16

--- play-30/src/test/scala/uk/gov/hmrc/webchat/services/WebChatVerificationServiceSpec.scala
+++ play-30/src/test/scala/uk/gov/hmrc/webchat/services/WebChatVerificationServiceSpec.scala
@@ -1,12 +1,28 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.webchat.services
 
 import org.mockito.ArgumentMatchers.{any => anyArg}
-import org.mockito.Mockito.*
+import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.matchers.must.Matchers.mustBe
+import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatest.wordspec.AnyWordSpec
-import play.api.mvc.AnyContentAsEmpty
+import play.api.mvc.{AnyContentAsEmpty, Request}
 import play.api.test.FakeRequest
 import uk.gov.hmrc.auth.core.ConfidenceLevel.L250
 import uk.gov.hmrc.auth.core.MissingBearerToken
@@ -21,11 +37,11 @@ import scala.concurrent.{ExecutionContext, Future}
 class WebChatVerificationServiceSpec
   extends AnyWordSpec
     with MockitoSugar
-    with ScalaFutures {
+    with ScalaFutures
+    with Matchers {
 
   implicit val ec: ExecutionContext = ExecutionContext.global
   implicit val hc: HeaderCarrier = HeaderCarrier()
-
 
   "verifyUser" should {
 
@@ -33,22 +49,26 @@ class WebChatVerificationServiceSpec
 
       val (service, userProfileProvider, verificationConnector) = createService()
 
-      implicit val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest("GET", "/")
+      implicit val request: Request[AnyContentAsEmpty.type] =
+        FakeRequest("GET", "/")
 
-      val profile = UserProfile(
-        "test-user-id",
-        Seq(
-          UserEnrolment(
-            "HMRC-MTD-IT",
-            "Activated",
-            Map("MTDITID" -> "12345")
-          )
-        ),
-        confidenceLevel = L250
-      )
+      val profile =
+        UserProfile(
+          "test-user-id",
+          Seq(
+            UserEnrolment(
+              "HMRC-MTD-IT",
+              "Activated",
+              Map("MTDITID" -> "12345")
+            )
+          ),
+          confidenceLevel = L250
+        )
 
       when(
-        userProfileProvider.retrieveUserProfile(anyArg[String])(anyArg())
+        userProfileProvider.retrieveUserProfile(
+          anyArg[String]
+        )(anyArg[Request[_]])
       ).thenReturn(
         Future.successful(profile)
       )
@@ -61,27 +81,26 @@ class WebChatVerificationServiceSpec
         Future.successful(HttpResponse(200))
       )
 
-
       service.verifyUser("test-user-id").futureValue
 
-
-      verify(verificationConnector)
-        .sendVerificationDetails(
-          anyArg[UserVerificationRequest]
-        )(anyArg[HeaderCarrier])
+      verify(verificationConnector).sendVerificationDetails(
+        anyArg[UserVerificationRequest]
+      )(anyArg[HeaderCarrier])
     }
 
     "not fail when authentication fails" in {
 
       val (service, userProfileProvider, verificationConnector) = createService()
 
-      implicit val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest("GET", "/")
+      implicit val request: Request[AnyContentAsEmpty.type] =
+        FakeRequest("GET", "/")
 
       when(
-        userProfileProvider.retrieveUserProfile(anyArg[String])(anyArg())
+        userProfileProvider.retrieveUserProfile(
+          anyArg[String]
+        )(anyArg[Request[_]])
       ).thenReturn(
-        Future.failed(MissingBearerToken()
-        )
+        Future.failed(MissingBearerToken())
       )
 
       service.verifyUser("test-user-id").futureValue mustBe ()
@@ -94,10 +113,13 @@ class WebChatVerificationServiceSpec
       val (service, userProfileProvider, verificationConnector) =
         createService()
 
-      implicit val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest("GET", "/")
+      implicit val request: Request[AnyContentAsEmpty.type] =
+        FakeRequest("GET", "/")
 
       when(
-        userProfileProvider.retrieveUserProfile(anyArg[String])(anyArg())
+        userProfileProvider.retrieveUserProfile(
+          anyArg[String]
+        )(anyArg[Request[_]])
       ).thenReturn(
         Future.failed(
           new TimeoutException("Identity retrieval timeout")
@@ -114,10 +136,13 @@ class WebChatVerificationServiceSpec
       val (service, userProfileProvider, verificationConnector) =
         createService()
 
-      implicit val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest("GET", "/")
+      implicit val request: Request[AnyContentAsEmpty.type] =
+        FakeRequest("GET", "/")
 
       when(
-        userProfileProvider.retrieveUserProfile(anyArg[String])(anyArg())
+        userProfileProvider.retrieveUserProfile(
+          anyArg[String]
+        )(anyArg[Request[_]])
       ).thenReturn(
         Future.failed(
           UpstreamErrorResponse(
@@ -128,40 +153,48 @@ class WebChatVerificationServiceSpec
         )
       )
 
-      service.verifyUser("test-user-id").futureValue mustBe()
+      service.verifyUser("test-user-id").futureValue mustBe ()
 
       verifyNoInteractions(verificationConnector)
     }
+
 
     "handle unexpected identity retrieval errors" in {
 
       val (service, userProfileProvider, verificationConnector) =
         createService()
 
-      implicit val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest("GET", "/")
+      implicit val request: Request[AnyContentAsEmpty.type] =
+        FakeRequest("GET", "/")
 
       when(
-        userProfileProvider.retrieveUserProfile(anyArg[String])(anyArg())
+        userProfileProvider.retrieveUserProfile(
+          anyArg[String]
+        )(anyArg[Request[_]])
       ).thenReturn(
         Future.failed(
           new RuntimeException("Invalid upstream response")
         )
       )
 
-      service.verifyUser("test-user-id").futureValue mustBe()
+      service.verifyUser("test-user-id").futureValue mustBe ()
 
       verifyNoInteractions(verificationConnector)
     }
   }
 
-  private def createService(): (WebChatVerificationService, UserProfileProvider, VerificationConnector) = {
+
+  private def createService()
+  : (WebChatVerificationService, UserProfileProvider, VerificationConnector) = {
+
     val userProfileProvider = mock[UserProfileProvider]
     val verificationConnector = mock[VerificationConnector]
 
-    val service = new WebChatVerificationService(
-      userProfileProvider,
-      verificationConnector
-    )
+    val service =
+      new WebChatVerificationService(
+        userProfileProvider,
+        verificationConnector
+      )
 
     (service, userProfileProvider, verificationConnector)
   }
